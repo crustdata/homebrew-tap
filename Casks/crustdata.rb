d7c18ad34216ece7a358189096cf9a97c2bc0601
Bhav Beri <bhav@crustdata.co>
2026-06-18 20:11:52 +0530

    Temporarily pin cask to 0.3.1 for update-flow testing

--- Casks/crustdata.rb
+++ Casks/crustdata.rb
@@ -1,6 +1,6 @@
 cask "crustdata" do
-  version "0.4.0"
-  sha256 "b48df56c419f0180f8132f241aaedb0f630f0c2bf75f33f31ae3c483362dbeb6"
+  version "0.3.1"
+  sha256 "6e593f65b65675a866f47cc84160531ecff85992fd714a1e6943b55bae3f1e45"
 
   url "https://github.com/bhav-cd/homebrew-tap/releases/download/v#{version}/Crustdata_#{version}_aarch64.dmg",
       verified: "github.com/bhav-cd/homebrew-tap/"
